f68bc16688091922558ed022c22f768f847d6508
luisarmandodv <luisarmandodv@gmail.com>
2018-09-05 17:52:05 -0500

    Add Bar Recognition and Face Detector funtionality

--- App.js
+++ App.js
@@ -1,29 +1,349 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Slider,
+  Alert,
+  Dimensions,
+  Image
+} from "react-native";
+import { RNCamera } from "react-native-camera";
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+const landmarkSize = 2;
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const flashModeOrder = {
+  off: "on",
+  on: "auto",
+  auto: "torch",
+  torch: "off"
+};
 
-type Props = {};
-export default class App extends Component<Props> {
+const wbOrder = {
+  auto: "sunny",
+  sunny: "cloudy",
+  cloudy: "shadow",
+  shadow: "fluorescent",
+  fluorescent: "incandescent",
+  incandescent: "auto"
+};
+
+export default class CameraScreen extends React.Component {
+  state = {
+    flash: "off",
+    zoom: 0,
+    autoFocus: "on",
+    depth: 0,
+    type: "back",
+    whiteBalance: "auto",
+    ratio: "16:9",
+    ratios: [],
+    photos: [],
+    faces: [],
+    scannerStatus: true,
+    flashStatus: false,
+    scanResultCode: "",
+    path: null
+  };
+
+  getRatios = async function() {
+    const ratios = await this.camera.getSupportedRatios();
+    return ratios;
+  };
+
+  toggleFacing() {
+    this.setState({
+      type: this.state.type === "back" ? "front" : "back"
+    });
+  }
+
+  toggleFlash() {
+    this.setState({
+      flash: flashModeOrder[this.state.flash]
+    });
+  }
+
+  toggleWB() {
+    this.setState({
+      whiteBalance: wbOrder[this.state.whiteBalance]
+    });
+  }
+
+  toggleFocus() {
+    this.setState({
+      autoFocus: this.state.autoFocus === "on" ? "off" : "on"
+    });
+  }
+
+  zoomOut() {
+    this.setState({
+      zoom: this.state.zoom - 0.1 < 0 ? 0 : this.state.zoom - 0.1
+    });
+  }
+
+  zoomIn() {
+    this.setState({
+      zoom: this.state.zoom + 0.1 > 1 ? 1 : this.state.zoom + 0.1
+    });
+  }
+
+  setFocusDepth(depth) {
+    this.setState({
+      depth
+    });
+  }
+
+  takePicture = async function() {
+    if (this.camera) {
+      this.camera.takePictureAsync().then(data => {
+        this.setState({ path: data.uri });
+        console.log(data);
+      });
+    }
+  };
+
+  onBarCodeRead = result => {
+    if (result.data) {
+      this.setState({
+        scannerStatus: false
+      });
+      Alert.alert(
+        "Bar Code:",
+        result.data,
+        [
+          { text: "Cancel", onPress: () => this.restartScan() },
+          { text: "Confirm", onPress: () => this.confirmScan(result) }
+        ],
+        { cancelable: false }
+      );
+    }
+  };
+
+  confirmScan(result) {
+    this.setState({
+      scanResultCode: result.data,
+      scannerStatus: true
+    });
+  }
+
+  restartScan() {
+    this.setState({ scannerStatus: true });
+  }
+
+  onFacesDetected = ({ faces }) => this.setState({ faces });
+
+  onFaceDetectionError = state => console.warn("Faces detection error:", state);
+
+  renderFace({ bounds, faceID, rollAngle, yawAngle }) {
+    return (
+      <View
+        key={faceID}
+        transform={[
+          { perspective: 600 },
+          { rotateZ: `${rollAngle.toFixed(0)}deg` },
+          { rotateY: `${yawAngle.toFixed(0)}deg` }
+        ]}
+        style={[
+          styles.face,
+          {
+            ...bounds.size,
+            left: bounds.origin.x,
+            top: bounds.origin.y
+          }
+        ]}
+      >
+        <Text style={styles.faceText}>ID: {faceID}</Text>
+        <Text style={styles.faceText}>rollAngle: {rollAngle.toFixed(0)}</Text>
+        <Text style={styles.faceText}>yawAngle: {yawAngle.toFixed(0)}</Text>
+      </View>
+    );
+  }
+
+  renderLandmarksOfFace(face) {
+    const renderLandmark = position =>
+      position && (
+        <View
+          style={[
+            styles.landmark,
+            {
+              left: position.x - landmarkSize / 2,
+              top: position.y - landmarkSize / 2
+            }
+          ]}
+        />
+      );
+    return (
+      <View key={`landmarks-${face.faceID}`}>
+        {renderLandmark(face.leftEyePosition)}
+        {renderLandmark(face.rightEyePosition)}
+        {renderLandmark(face.leftEarPosition)}
+        {renderLandmark(face.rightEarPosition)}
+        {renderLandmark(face.leftCheekPosition)}
+        {renderLandmark(face.rightCheekPosition)}
+        {renderLandmark(face.leftMouthPosition)}
+        {renderLandmark(face.mouthPosition)}
+        {renderLandmark(face.rightMouthPosition)}
+        {renderLandmark(face.noseBasePosition)}
+        {renderLandmark(face.bottomMouthPosition)}
+      </View>
+    );
+  }
+
+  renderFaces() {
+    return (
+      <View style={styles.facesContainer} pointerEvents="none">
+        {this.state.faces.map(this.renderFace)}
+      </View>
+    );
+  }
+
+  renderLandmarks() {
+    return (
+      <View style={styles.facesContainer} pointerEvents="none">
+        {this.state.faces.map(this.renderLandmarksOfFace)}
+      </View>
+    );
+  }
+
+  renderBarcode() {
+    return (
+      <View style={styles.facesContainer} pointerEvents="none">
+        {this.state.faces.map(this.renderFace)}
+      </View>
+    );
+  }
+
+  renderCamera() {
+    return (
+      <RNCamera
+        ref={ref => {
+          this.camera = ref;
+        }}
+        style={{
+          flex: 1
+        }}
+        type={this.state.type}
+        flashMode={this.state.flash}
+        autoFocus={this.state.autoFocus}
+        zoom={this.state.zoom}
+        whiteBalance={this.state.whiteBalance}
+        ratio={this.state.ratio}
+        faceDetectionLandmarks={RNCamera.Constants.FaceDetection.Landmarks.all}
+        onBarCodeRead={this.onBarCodeRead}
+        onFacesDetected={this.onFacesDetected}
+        onFaceDetectionError={this.onFaceDetectionError}
+        focusDepth={this.state.depth}
+        permissionDialogTitle={"Permission to use camera"}
+        permissionDialogMessage={
+          "We need your permission to use your camera phone"
+        }
+      >
+        <View
+          style={{
+            flex: 0.5,
+            backgroundColor: "transparent",
+            flexDirection: "row",
+            justifyContent: "space-around"
+          }}
+        >
+          <TouchableOpacity
+            style={styles.flipButton}
+            onPress={this.toggleFacing.bind(this)}
+          >
+            <Text style={styles.flipText}> FLIP </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.flipButton}
+            onPress={this.toggleFlash.bind(this)}
+          >
+            <Text style={styles.flipText}> FLASH: {this.state.flash} </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.flipButton}
+            onPress={this.toggleWB.bind(this)}
+          >
+            <Text style={styles.flipText}> WB: {this.state.whiteBalance} </Text>
+          </TouchableOpacity>
+        </View>
+        <View
+          style={{
+            flex: 0.4,
+            backgroundColor: "transparent",
+            flexDirection: "row",
+            alignSelf: "flex-end"
+          }}
+        >
+          <Slider
+            style={{ width: 150, marginTop: 15, alignSelf: "flex-end" }}
+            onValueChange={this.setFocusDepth.bind(this)}
+            step={0.1}
+            disabled={this.state.autoFocus === "on"}
+          />
+        </View>
+        <View
+          style={{
+            flex: 0.1,
+            backgroundColor: "transparent",
+            flexDirection: "row",
+            alignSelf: "flex-end"
+          }}
+        >
+          <TouchableOpacity
+            style={[styles.flipButton, { flex: 0.1, alignSelf: "flex-end" }]}
+            onPress={this.zoomIn.bind(this)}
+          >
+            <Text style={styles.flipText}> + </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.flipButton, { flex: 0.1, alignSelf: "flex-end" }]}
+            onPress={this.zoomOut.bind(this)}
+          >
+            <Text style={styles.flipText}> - </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.flipButton, { flex: 0.25, alignSelf: "flex-end" }]}
+            onPress={this.toggleFocus.bind(this)}
+          >
+            <Text style={styles.flipText}> AF : {this.state.autoFocus} </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[
+              styles.flipButton,
+              styles.picButton,
+              { flex: 0.3, alignSelf: "flex-end" }
+            ]}
+            onPress={this.takePicture.bind(this)}
+          >
+            <Text style={styles.flipText}> SNAP </Text>
+          </TouchableOpacity>
+        </View>
+        {this.renderFaces()}
+        {this.renderLandmarks()}
+      </RNCamera>
+    );
+  }
+
+  renderImage() {
+    return (
+      <View>
+        <Image source={{ uri: this.state.path }} style={styles.preview} />
+        <Text
+          style={styles.cancel}
+          onPress={() => this.setState({ path: null })}
+        >
+          Cancel
+        </Text>
+      </View>
+    );
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View
+        style={this.state.path ? styles.previewContainer : styles.container}
+      >
+        {this.state.path ? this.renderImage() : this.renderCamera()}
       </View>
     );
   }
@@ -32,18 +352,95 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    paddingTop: 10,
+    backgroundColor: "#000"
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
+  previewContainer: {
+    flex: 1,
+    paddingTop: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    alignSelf: "stretch",
+    backgroundColor: "#000"
+  },
+  cancel: {
+    position: "absolute",
+    right: 20,
+    top: 20,
+    backgroundColor: "transparent",
+    color: "#FFF",
+    fontWeight: "600",
+    fontSize: 17
+  },
+  preview: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    height: Dimensions.get("window").height,
+    width: Dimensions.get("window").width
+  },
+  navigation: {
+    flex: 1
+  },
+  flipButton: {
+    flex: 0.3,
+    height: 40,
+    marginHorizontal: 2,
+    marginBottom: 10,
+    marginTop: 20,
+    borderRadius: 8,
+    borderColor: "white",
+    borderWidth: 1,
+    padding: 5,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  flipText: {
+    color: "white",
+    fontSize: 15
+  },
+  item: {
+    margin: 4,
+    backgroundColor: "indianred",
+    height: 35,
+    width: 80,
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  picButton: {
+    backgroundColor: "darkseagreen"
+  },
+  facesContainer: {
+    position: "absolute",
+    bottom: 0,
+    right: 0,
+    left: 0,
+    top: 0
+  },
+  face: {
+    padding: 10,
+    borderWidth: 2,
+    borderRadius: 2,
+    position: "absolute",
+    borderColor: "#FFD700",
+    justifyContent: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)"
+  },
+  landmark: {
+    width: landmarkSize,
+    height: landmarkSize,
+    position: "absolute",
+    backgroundColor: "red"
+  },
+  faceText: {
+    color: "#FFD700",
+    fontWeight: "bold",
+    textAlign: "center",
     margin: 10,
+    backgroundColor: "transparent"
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  row: {
+    flexDirection: "row"
+  }
 });
